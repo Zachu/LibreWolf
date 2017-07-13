a43de879d577cabea73b2a1d5288255243e0a890
Jani Korhonen <zachu@thegroup.fi>
2017-07-13 19:08:29 +0300

    Add child info

--- lang/fi/roles/child.php
+++ lang/fi/roles/child.php
@@ -2,4 +2,5 @@
     'template' => 'villager',
     'name'     => 'Lapsi',
     'special'  => 'Saat kurkkia yöllä kun ihmissudet suunnittelevat metsästysretkeä. Kuolet kauhusta jos ihmissudet huomaavat sinut.',
+    'info'     => 'Ihmissudet voivat yöllä näyttää pelinjohtajalle itkuelettä ja osoittaa pelaajaa, joka on paljastunut lapseksi. Pelinjohtaja ilmoittaa aamulla pelaajan löytyneen kuolleena.',
 ];
