ee2bcf04c934d6dc945933783b551a3ab81b45e2
Jani Korhonen <zachu@thegroup.fi>
2017-07-13 19:08:47 +0300

    Add info for diseased

--- lang/fi/roles/diseased.php
+++ lang/fi/roles/diseased.php
@@ -2,4 +2,5 @@
     'template' => 'villager',
     'name'     => 'Sairas',
     'special'  => 'Jos ihmissudet syövät sinut, he eivät saa syödä seuraavana yönä.',
+    'info'     => 'Tämä koskee kaikkia susia, myös Alfasutta.',
 ];
