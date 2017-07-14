671e5373ba0be45d2a1316b49281a0deadb4f73c
Jani Korhonen <zachu@thegroup.fi>
2017-07-15 00:13:42 +0300

    Separate a RoleRepository from a common Repository

---
+++ src/Repository.php
@@ -0,0 +1,125 @@
+<?php namespace zachu\LibreWolf;
+
+class Repository implements \Iterator
+{
+    protected $items;
+
+    protected $model;
+
+    private $position = 0;
+
+    public function __construct(array $items = [])
+    {
+        $this->items = $items;
+    }
+
+    public function count()
+    {
+        return count($this->items);
+    }
+
+    public function create(array $attributes = [])
+    {
+        $this->items[] = new $this->model($attributes);
+    }
+
+    public function current()
+    {
+        return $this->items[$this->position];
+    }
+
+    public function find($id)
+    {
+        foreach ($this->items as $item) {
+            if ($item->id == $id) {
+                return $item;
+            }
+        }
+    }
+
+    public function findBy($column, $value)
+    {
+        $found = [];
+        foreach ($this->items as $item) {
+            if ($item->$column === $value) {
+                $found[] = $item;
+            }
+        }
+
+        return $this->withItems($found);
+    }
+
+    public function findByIsNotSet($column)
+    {
+        $found = [];
+        foreach ($this->items as $item) {
+            if (!isset($item->$column)) {
+                $found[] = $item;
+            }
+        }
+
+        return $this->withItems($found);
+    }
+
+    public function findByIsSet($column)
+    {
+        $found = [];
+        foreach ($this->items as $item) {
+            if (isset($item->$column)) {
+                $found[] = $item;
+            }
+        }
+
+        return $this->withItems($found);
+    }
+
+    public function findByNot($column, $value)
+    {
+        $found = [];
+        foreach ($this->items as $item) {
+            if ($item->$column !== $value) {
+                $found[] = $item;
+            }
+        }
+
+        return $this->withItems($found);
+    }
+
+    public function key()
+    {
+        return $this->position;
+    }
+
+    public function next()
+    {
+        $this->position++;
+        if ($this->valid()) {
+            return $this->current();
+        }
+    }
+
+    public function paginate($limit = 15, $skip = 0)
+    {
+        return $this->withItems(array_slice($this->items, $skip, $limit, true));
+    }
+
+    public function rewind()
+    {
+        $this->position = 0;
+    }
+
+    public function toArray()
+    {
+        return $this->items;
+    }
+
+    public function valid()
+    {
+        return isset($this->items[$this->position]);
+    }
+
+    private function withItems(array $items = [])
+    {
+        return new self($items);
+    }
+}
--- src/RoleRepository.php
+++ src/RoleRepository.php
@@ -1,123 +1,33 @@
 <?php namespace zachu\LibreWolf;
 
 use zachu\LibreWolf\Role;
+use zachu\LibreWolf\Repository;
 use Illuminate\Contracts\Translation\Translator;
 
-class RoleRepository implements \Iterator
+class RoleRepository extends Repository
 {
-    private $position = 0;
-    protected $items;
-    protected $translator;
     protected $model = Role::class;
+
+    protected $translator;
 
     public function __construct(Translator $translator, array $items = [])
     {
-        $this->items      = $items;
+        parent::__construct($items);
         $this->translator = $translator;
     }
 
-    public function all()
+    public function create(array $attributes = [])
+    {
+        $this->items[] = new $this->model($this->translator, $attributes);
+    }
+
+    public function toArray()
     {
         return $this->items;
     }
 
-    public function paginate($limit = 15, $skip = 0)
+    public function withItems(array $items = [])
     {
-        return array_slice($this->items, $skip, $limit, true);
-    }
-
-    public function find($id)
-    {
-        foreach ($this->items as $item) {
-            if ($item->id == $id) {
-                return $item;
-            }
-        }
-    }
-
-    public function findBy($column, $value)
-    {
-        $found = [];
-        foreach ($this->items as $item) {
-            if ($item->$column === $value) {
-                $found[] = $item;
-            }
-        }
-
-        return $found;
-    }
-
-    public function findByNot($column, $value)
-    {
-        $found = [];
-        foreach ($this->items as $item) {
-            if ($item->$column !== $value) {
-                $found[] = $item;
-            }
-        }
-
-        return $found;
-    }
-
-    public function findByIsSet($column)
-    {
-        $found = [];
-        foreach ($this->items as $item) {
-            if (isset($item->$column)) {
-                $found[] = $item;
-            }
-        }
-
-        return $found;
-    }
-
-    public function findByIsNotSet($column)
-    {
-        $found = [];
-        foreach ($this->items as $item) {
-            if (!isset($item->$column)) {
-                $found[] = $item;
-            }
-        }
-
-        return $found;
-    }
-
-    public function create($id, array $attributes = [])
-    {
-        $this->items[] = new $this->model($id, $this->translator, $attributes);
-    }
-
-    public function count()
-    {
-        return count($this->items);
-    }
-
-    public function rewind()
-    {
-        $this->position = 0;
-    }
-
-    public function current()
-    {
-        return $this->items[$this->position];
-    }
-
-    public function key()
-    {
-        return $this->position;
-    }
-
-    public function next()
-    {
-        $this->position++;
-        if ($this->valid()) {
-            return $this->current();
-        }
-    }
-
-    public function valid()
-    {
-        return isset($this->items[$this->position]);
+        return new self($this->translator, $items);
     }
 }
