f59426b2fffcce0eeb3bcf66b3336c12d31d22c7
Jani Korhonen <zachu@thegroup.fi>
2017-07-15 00:56:19 +0300

    Add layout structure

---
+++ views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="{{ $language }}">
+<head>
+    <meta charset="UTF-8">
+    <title>LibreWolf</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="assets/css/screen.css" media="screen">
+    <link rel="stylesheet" href="assets/css/print.css" media="print">
+</head>
+<body>
+
+</body>
+</html>
