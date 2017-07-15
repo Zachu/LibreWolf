9925dd2fa73a024946c5f0536acc6270bf8b61c4
Jani Korhonen <zachu@thegroup.fi>
2017-07-15 03:19:59 +0300

    Add some needed variables to be passed to templates

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php require 'init.php';
 
-echo $app['templating']->render('layout', [
-    'roles'    => $app['roles'],
-    'language' => $app['translator']->getLocale(),
+echo $app['template']->render('layout', [
+    'roles'           => $app['roles'],
+    'language'        => $app['translator']->getLocale(),
+    'template'        => $app['template'],
+    'skip_role_cards' => $app['config']->get('skip_role_cards'),
+    'skip_role_rules' => $app['config']->get('skip_role_rules'),
+    'role_card_count' => $app['role_card_count'],
+    'night_phases'    => $app['config']->get('night_phases'),
 ]);
